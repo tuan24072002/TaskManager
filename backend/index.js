cd1b8d94ceb7f7559275a77f24035e1f317a6684
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2025-03-23 20:04:08 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true
 }));
